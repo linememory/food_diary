dc3ccfc0e579a6d9e528bc631e46f7fdcffc5287
Lukas Leitner <lukasleitner17@gmail.com>
2022-01-11 14:22:25 +0100

    changed some colors

--- lib/core/themes/custom_theme.dart
+++ lib/core/themes/custom_theme.dart
@@ -9,23 +9,26 @@ class CustomTheme {
       colorScheme: ThemeData.light().colorScheme.copyWith(
             secondary: const Color(0xff64b5f6),
             primary: const Color(0xffc63f17),
+            background: const Color(0xffEEE9DE),
           ),
-      backgroundColor: Colors.white,
-      scaffoldBackgroundColor: Colors.white,
+      backgroundColor: const Color(0xffEEEEEE),
+      scaffoldBackgroundColor: const Color(0xffEEEEEE),
     );
   }
 
   static ThemeData darkTheme() {
     return ThemeData.dark().copyWith(
-        primaryColor: const Color(0xff202020),
-        primaryColorLight: const Color(0xff303030),
-        primaryColorDark: const Color(0xff101010),
-        colorScheme: ThemeData.dark().colorScheme.copyWith(
-              secondary: const Color(0xffc63f17),
-            ),
-        backgroundColor: Colors.grey,
-        scaffoldBackgroundColor: Colors.black54
-        //textTheme: TextTheme()
-        );
+      primaryColor: const Color(0xff79250d),
+      primaryColorLight: const Color(0xff922c0f),
+      primaryColorDark: const Color(0xff5e1c09),
+      colorScheme: ThemeData.dark().colorScheme.copyWith(
+            secondary: const Color(0xff305776),
+            primary: const Color(0xff601e0b),
+            background: const Color(0xff262523),
+          ),
+      backgroundColor: const Color(0xff222222),
+      scaffoldBackgroundColor: const Color(0xff222222),
+      //textTheme: TextTheme()
+    );
   }
 }
