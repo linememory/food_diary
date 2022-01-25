2113d17a832c3d689404f3b8ae7d9ead0f168fa1
Lukas Leitner <lukasleitner17@gmail.com>
2022-01-25 09:35:34 +0100

    cleaned up

--- lib/features/diary/data/models/food_dto.dart
+++ lib/features/diary/data/models/food_dto.dart
@@ -1,27 +1,5 @@
 import 'package:food_diary/features/diary/domain/value_objects/food.dart';
 
-// class FoodModel extends Food {
-//   const FoodModel({required String name, required Amount amount})
-//       : super(name: name, amount: amount);
-
-//   factory FoodModel.fromMap(Map<String, dynamic> map) {
-//     return FoodModel(
-//       name: map['name'],
-//       amount: (Amount.values[map['amount']]),
-//     );
-//   }
-
-//   Map<String, dynamic> toMap({int id = -1}) {
-//     return {
-//       'name': name,
-//       'amount': amount.index,
-//       'meal_id': id,
-//     };
-//   }
-
-//   FoodModel.from(Food food) : super(name: food.name, amount: food.amount);
-// }
-
 class FoodDTO {
   int? id;
   String name;
--- lib/features/diary/data/models/meal_dto.dart
+++ lib/features/diary/data/models/meal_dto.dart
@@ -3,42 +3,6 @@ import 'package:flutter/foundation.dart';
 import 'package:food_diary/features/diary/data/models/food_dto.dart';
 import 'package:food_diary/features/diary/domain/entities/meal.dart';
 
-// class MealModel extends Meal {
-//   const MealModel({
-//     int id = -1,
-//     required DateTime dateTime,
-//     required List<FoodModel> foods,
-//   }) : super(id: id, dateTime: dateTime, foods: foods);
-
-//   factory MealModel.fromMap(Map<String, dynamic> map) {
-//     return MealModel(
-//       id: map['_id'],
-//       dateTime: DateTime.fromMicrosecondsSinceEpoch(map['date_time']),
-//       foods: (map['foods'] as List<Map<String, dynamic>>)
-//           .map((e) => FoodModel.fromMap(e))
-//           .toList(),
-//     );
-//   }
-
-//   Map<String, dynamic> toMap() {
-//     return {
-//       'date_time': dateTime.microsecondsSinceEpoch,
-//       //'foods': foods.map((e) => (e as FoodModel).toMap()).toList()
-//     };
-//   }
-
-//   void addFood(FoodModel foodToAdd) {
-//     foods.add(foodToAdd);
-//   }
-
-//   static MealModel from(Meal meal) {
-//     return MealModel(
-//         id: meal.id,
-//         dateTime: meal.dateTime,
-//         foods: List.from(meal.foods.map((e) => FoodModel.from(e))));
-//   }
-// }
-
 class MealDTO {
   int? id;
   DateTime dateTime;
