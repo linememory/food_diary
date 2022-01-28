3ceda1482b4821a1d86a6eaf1e3744d2f413e1d6
Lukas Leitner <lukasleitner17@gmail.com>
2022-01-28 14:23:26 +0100

    added functionality for all types

--- lib/features/diary/presentation/widgets/entry_form.dart
+++ lib/features/diary/presentation/widgets/entry_form.dart
@@ -1,31 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:food_diary/features/diary/domain/entities/bowel_movement_entry.dart';
+import 'package:food_diary/features/diary/domain/entities/diary_entry.dart';
 import 'package:food_diary/features/diary/domain/entities/meal_entry.dart';
+import 'package:food_diary/features/diary/domain/entities/symptom_entry.dart';
+import 'package:food_diary/features/diary/domain/value_objects/bowel_movement.dart';
 import 'package:food_diary/features/diary/domain/value_objects/food.dart';
-import 'package:food_diary/features/diary/presentation/bloc/meal_form_bloc.dart';
+import 'package:food_diary/features/diary/domain/value_objects/symptom.dart';
+import 'package:food_diary/features/diary/presentation/cubit/bowel_movement_form_cubit.dart';
+import 'package:food_diary/features/diary/presentation/cubit/entry_form_cubit.dart';
+import 'package:food_diary/features/diary/presentation/cubit/meal_form_cubit.dart';
+import 'package:food_diary/features/diary/presentation/cubit/symptom_form_cubit.dart';
 import 'package:food_diary/injection_container.dart';
 import 'package:intl/intl.dart';
 
-enum FormType {
-  addMeal,
-  updateMeal,
-}
+class EntryForm extends StatelessWidget {
+  final DiaryEntry entry;
 
-class MealForm extends StatelessWidget {
-  final FormType type;
-  final MealEntry? meal;
-
-  const MealForm({Key? key, this.type = FormType.addMeal, this.meal})
-      : super(key: key);
+  const EntryForm({Key? key, required this.entry}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    String buttonText = type == FormType.addMeal ? "Add" : "Update";
-    return BlocProvider<MealFormBloc>(
-      create: (context) => MealFormBloc(sl(), meal),
-      child: BlocListener<MealFormBloc, MealFormState>(
+    String buttonText = entry.id == null ? "Add" : "Update";
+    return BlocProvider<EntryFormCubit>(
+      create: (context) => EntryFormCubit(sl()),
+      child: BlocListener<EntryFormCubit, EntryFormState>(
         listener: (context, state) {
-          if (state is MealFormSubmitFailed) {
+          if (state is EntryFormSubmitFailed) {
             ScaffoldMessenger.of(context)
                 .showSnackBar(SnackBar(content: Text(state.message)));
           }
@@ -38,105 +39,8 @@ class MealForm extends StatelessWidget {
             body: Column(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Expanded(
-                  child: BlocBuilder<MealFormBloc, MealFormState>(
-                    buildWhen: (previous, current) =>
-                        true, //current is MealFormSubmitted,
-                    builder: (context, state) {
-                      return Column(
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: GestureDetector(
-                              onTap: () async {
-                                final DateTime? pickedDate =
-                                    await showDatePicker(
-                                        context: context,
-                                        initialDate: state.dateTime,
-                                        initialDatePickerMode:
-                                            DatePickerMode.day,
-                                        firstDate: DateTime(2015),
-                                        lastDate: DateTime(2101));
-                                final TimeOfDay? pickedTime =
-                                    await showTimePicker(
-                                  context: context,
-                                  initialTime:
-                                      TimeOfDay.fromDateTime(state.dateTime),
-                                );
-                                if (pickedDate != null) {
-                                  DateTime dateTime = DateTime(
-                                    pickedDate.year,
-                                    pickedDate.month,
-                                    pickedDate.day,
-                                    pickedTime?.hour ?? 0,
-                                    pickedTime?.minute ?? 0,
-                                  );
-                                  BlocProvider.of<MealFormBloc>(context)
-                                      .add(MealFormDateTimeChanged(dateTime));
-                                }
-                              },
-                              child: Text(
-                                DateFormat('EEE, dd.MM.yyy')
-                                    .format(state.dateTime),
-                                style: Theme.of(context).textTheme.subtitle1,
-                              ),
-                            ),
-                          ),
-                          Expanded(
-                            child: SingleChildScrollView(
-                              child: Container(
-                                margin: const EdgeInsets.all(10),
-                                padding: const EdgeInsets.all(2),
-                                decoration: BoxDecoration(
-                                  borderRadius: const BorderRadius.all(
-                                      Radius.circular(10.0)),
-                                  border: Border.all(
-                                    color:
-                                        Theme.of(context).colorScheme.secondary,
-                                    width: 2.0,
-                                  ),
-                                ),
-                                child: Column(
-                                  children: state.foods
-                                      .asMap()
-                                      .entries
-                                      .map((e) =>
-                                          FoodEntry(id: e.key, food: e.value))
-                                      .toList(),
-                                ),
-                              ),
-                            ),
-                          ),
-                        ],
-                      );
-                    },
-                  ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    ElevatedButton(
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                      child: const Text("Cancel"),
-                    ),
-                    BlocListener<MealFormBloc, MealFormState>(
-                      listener: (context, state) {
-                        if (state is MealFormSubmitted) {
-                          Navigator.pop(context);
-                        } else if (state is MealFormSubmitFailed) {}
-                      },
-                      child: ElevatedButton(
-                        onPressed: () {
-                          BlocProvider.of<MealFormBloc>(context)
-                              .add(MealFormSubmit());
-                        },
-                        child: Text(buttonText),
-                      ),
-                    ),
-                  ],
-                ),
+                InputFields(entry: entry),
+                FormButtons(buttonText: buttonText),
               ],
             ),
           );
@@ -146,33 +50,199 @@ class MealForm extends StatelessWidget {
   }
 }
 
-class FoodEntry extends StatefulWidget {
-  FoodEntry({Key? key, required this.food, required this.id}) : super(key: key);
+class InputFields extends StatelessWidget {
+  const InputFields({
+    Key? key,
+    required this.entry,
+  }) : super(key: key);
+
+  final DiaryEntry entry;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Column(
+        children: [
+          DateTimePicker(
+              dateTime: entry.dateTime, onNewDate: (newDateTime) => null),
+          Expanded(
+            child: SingleChildScrollView(
+              child: Container(
+                margin: const EdgeInsets.all(10),
+                padding: const EdgeInsets.all(2),
+                decoration: BoxDecoration(
+                  borderRadius: const BorderRadius.all(Radius.circular(10.0)),
+                  border: Border.all(
+                    color: Theme.of(context).colorScheme.secondary,
+                    width: 2.0,
+                  ),
+                ),
+                child: _inputFields(),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  BlocProvider _inputFields() {
+    if (entry is MealEntry) {
+      return BlocProvider<MealFormCubit>(
+        create: (context) => MealFormCubit(),
+        child: Column(
+          children: (entry as MealEntry)
+              .foods
+              .asMap()
+              .entries
+              .map((e) => FoodField(id: e.key, food: e.value))
+              .toList(),
+        ),
+      );
+    } else if (entry is SymptomEntry) {
+      return BlocProvider<MealFormCubit>(
+        create: (context) => MealFormCubit(),
+        child: Column(
+          children: (entry as SymptomEntry)
+              .symptoms
+              .asMap()
+              .entries
+              .map((e) => SymptomField(
+                    id: e.key,
+                    symptom: e.value,
+                  ))
+              .toList(),
+        ),
+      );
+    } else if (entry is BowelMovementEntry) {
+      return BlocProvider<MealFormCubit>(
+          create: (context) => MealFormCubit(),
+          child: BowelMovementField(
+              bowelMovement: (entry as BowelMovementEntry).bowelMovement));
+    } else {
+      throw (Exception("Not a valid type"));
+    }
+  }
+}
+
+class FormButtons extends StatelessWidget {
+  const FormButtons({
+    Key? key,
+    required this.buttonText,
+  }) : super(key: key);
+
+  final String buttonText;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text("Cancel"),
+        ),
+        BlocConsumer<EntryFormCubit, EntryFormState>(
+          listener: (context, state) {
+            if (state is EntryFormSubmitted) {
+              Navigator.pop(context);
+            }
+          },
+          builder: (context, state) {
+            return ElevatedButton(
+              onPressed: state is EntryFormValid
+                  ? () {
+                      BlocProvider.of<EntryFormCubit>(context).submit(
+                          MealEntry(dateTime: DateTime.now(), foods: []));
+                    }
+                  : null,
+              child: Text(buttonText),
+            );
+          },
+        ),
+      ],
+    );
+  }
+}
+
+class DateTimePicker extends StatelessWidget {
+  const DateTimePicker({
+    Key? key,
+    required this.dateTime,
+    required this.onNewDate,
+  }) : super(key: key);
+
+  final DateTime dateTime;
+
+  final void Function(DateTime dateTime) onNewDate;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 20.0, bottom: 5),
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 2),
+        decoration: BoxDecoration(
+          borderRadius: const BorderRadius.all(Radius.circular(20)),
+          color: Theme.of(context).colorScheme.secondary,
+        ),
+        child: GestureDetector(
+          onTap: () async {
+            final DateTime? pickedDate = await showDatePicker(
+                context: context,
+                initialDate: dateTime,
+                initialDatePickerMode: DatePickerMode.day,
+                firstDate: DateTime(2015),
+                lastDate: DateTime(2101));
+            final TimeOfDay? pickedTime = await showTimePicker(
+              context: context,
+              initialTime: TimeOfDay.fromDateTime(dateTime),
+            );
+            if (pickedDate != null) {
+              DateTime newDateTime = DateTime(
+                pickedDate.year,
+                pickedDate.month,
+                pickedDate.day,
+                pickedTime?.hour ?? 0,
+                pickedTime?.minute ?? 0,
+              );
+              onNewDate(newDateTime);
+              // BlocProvider.of<MealFormBloc>(context)
+              //     .add(MealFormDateTimeChanged(newDateTime));
+            }
+          },
+          child: Text(
+            DateFormat('EEE, dd.MM.yyy  hh:mm').format(dateTime),
+            style: Theme.of(context).textTheme.subtitle1,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class FoodField extends StatelessWidget {
+  FoodField({Key? key, required this.food, required this.id}) : super(key: key);
+
   final Food food;
   final int id;
   final TextEditingController controller = TextEditingController();
 
   @override
-  State<FoodEntry> createState() => _FoodEntryState();
-}
-
-class _FoodEntryState extends State<FoodEntry> {
-  @override
   Widget build(BuildContext context) {
-    widget.controller.text = widget.food.name;
-    widget.controller.selection = TextSelection(
-        baseOffset: widget.food.name.length,
-        extentOffset: widget.food.name.length);
-
+    controller.text = food.name;
+    controller.selection = TextSelection(
+        baseOffset: food.name.length, extentOffset: food.name.length);
     return Row(
       children: [
         Expanded(
           child: TextField(
             onChanged: (text) {
-              BlocProvider.of<MealFormBloc>(context)
-                  .add(MealFormNameChanged(widget.id, text));
+              BlocProvider.of<MealFormCubit>(context).nameChanged(id, text);
             },
-            controller: widget.controller,
+            controller: controller,
             decoration: const InputDecoration(
               contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 10),
               border: OutlineInputBorder(
@@ -186,10 +256,10 @@ class _FoodEntryState extends State<FoodEntry> {
         Padding(
           padding: const EdgeInsets.symmetric(horizontal: 8.0),
           child: DropdownButton<Amount>(
-            value: widget.food.amount,
+            value: food.amount,
             onChanged: (Amount? amount) {
-              BlocProvider.of<MealFormBloc>(context).add(
-                  MealFormAmountChanged(widget.id, amount ?? Amount.small));
+              BlocProvider.of<MealFormCubit>(context)
+                  .amountChanged(id, amount ?? Amount.small);
             },
             items: const [
               DropdownMenuItem<Amount>(
@@ -205,6 +275,142 @@ class _FoodEntryState extends State<FoodEntry> {
                 value: Amount.high,
               ),
             ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class SymptomField extends StatelessWidget {
+  SymptomField({Key? key, required this.symptom, required this.id})
+      : super(key: key);
+
+  final Symptom symptom;
+  final int id;
+  final TextEditingController controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    controller.text = symptom.name;
+    controller.selection = TextSelection(
+        baseOffset: symptom.name.length, extentOffset: symptom.name.length);
+    return Row(
+      children: [
+        Expanded(
+          child: TextField(
+            onChanged: (text) {
+              BlocProvider.of<SymptomFormCubit>(context).nameChanged(id, text);
+            },
+            controller: controller,
+            decoration: const InputDecoration(
+              contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+              ),
+            ),
+            maxLines: 1,
+            minLines: 1,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+          child: DropdownButton<Intensity>(
+            value: symptom.intensity,
+            onChanged: (Intensity? intensity) {
+              BlocProvider.of<SymptomFormCubit>(context)
+                  .intensityChanged(id, intensity ?? Intensity.low);
+            },
+            items: const [
+              DropdownMenuItem<Intensity>(
+                child: Text("Small"),
+                value: Intensity.low,
+              ),
+              DropdownMenuItem<Intensity>(
+                child: Text("Medium"),
+                value: Intensity.medium,
+              ),
+              DropdownMenuItem<Intensity>(
+                child: Text("High"),
+                value: Intensity.high,
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class BowelMovementField extends StatelessWidget {
+  BowelMovementField({Key? key, required this.bowelMovement}) : super(key: key);
+
+  final BowelMovement bowelMovement;
+  final TextEditingController controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    controller.text = bowelMovement.note;
+    controller.selection = TextSelection(
+        baseOffset: bowelMovement.note.length,
+        extentOffset: bowelMovement.note.length);
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+          child: DropdownButton<StoolType>(
+            value: bowelMovement.stoolType,
+            onChanged: (StoolType? type) {
+              BlocProvider.of<BowelMovementFormCubit>(context)
+                  .typeChanged(type ?? StoolType.type1);
+            },
+            items: const [
+              DropdownMenuItem<StoolType>(
+                child: Text("Type 1"),
+                value: StoolType.type1,
+              ),
+              DropdownMenuItem<StoolType>(
+                child: Text("Type 2"),
+                value: StoolType.type2,
+              ),
+              DropdownMenuItem<StoolType>(
+                child: Text("Type 3"),
+                value: StoolType.type3,
+              ),
+              DropdownMenuItem<StoolType>(
+                child: Text("Type 4"),
+                value: StoolType.type3,
+              ),
+              DropdownMenuItem<StoolType>(
+                child: Text("Type 5"),
+                value: StoolType.type3,
+              ),
+              DropdownMenuItem<StoolType>(
+                child: Text("Type 6"),
+                value: StoolType.type3,
+              ),
+              DropdownMenuItem<StoolType>(
+                child: Text("Type 7"),
+                value: StoolType.type3,
+              ),
+            ],
+          ),
+        ),
+        Expanded(
+          child: TextField(
+            onChanged: (text) {
+              BlocProvider.of<BowelMovementFormCubit>(context)
+                  .noteChanged(text);
+            },
+            controller: controller,
+            decoration: const InputDecoration(
+              contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+              ),
+            ),
+            maxLines: 1,
+            minLines: 1,
           ),
         ),
       ],
